08d21de14ceddb569a9345316883cbd4c259e277
bobo <1102158249@qq.com>
2024-04-18 16:15:43 +0800

    第4次提

--- src/main/java/org/hongda/git/GitApply.java
+++ src/main/java/org/hongda/git/GitApply.java
@@ -10,4 +10,14 @@ public class GitApply {
     public  void test1() {
         System.out.println("git第一次提交");
     }
+
+
+    public  void test2() {
+        System.out.println("git第2次提交");
+    }
+
+
+    public  void test3() {
+        System.out.println("git第3次提交");
+    }
 }
