3639d05cd2fc5581f60454e730e18ac45676bd6c
bobo <1102158249@qq.com>
2024-04-18 14:19:17 +0800

    第一次提交

---
+++ src/main/java/org/hongda/git/GitApply.java
@@ -0,0 +1,13 @@
+package org.hongda.git;
+
+/**
+ * @ClassName GitApply
+ * @Description 操作git的应用
+ * @Author liuyibo
+ * @Date 2024/4/18 14:14
+ **/
+public class GitApply {
+    public  void test1() {
+        System.out.println("git第一次提交");
+    }
+}
